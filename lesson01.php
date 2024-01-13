34ad24aed2e8f94b43d5294098188e235e64f013
Romar Marcos <romar.marcos96@gmail.com>
2024-01-13 23:15:23 +0800

    first commit

---
+++ lesson01.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Lesson 1</title>
+</head>
+<body>
+<?php 
+#This is a single line comment
+echo "this echo comes after a single line comment";
+
+/* Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde ea, explicabo corrupti iure officiis quis quas veritatis harum incidunt repellendus vel. Repellendus, adipisci. Ratione iusto deleniti praesentium illum hic delectus. */
+
+echo "<br>this echo comes after a multiline comment";
+?>
+</body>
+</html>
