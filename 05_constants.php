82c26e039964f2f0c9b60e7121205cae7fc5dbe0
Romar Marcos <romar.marcos96@gmail.com>
2024-01-14 12:36:24 +0800

    finished 05

--- 05_constants.php
+++ 05_constants.php
@@ -6,6 +6,45 @@
     <title>Constants</title>
 </head>
 <body>
-    
+    <?php
+        //Constants
+        define("ONE",1);
+        define("Cats","The only creatures on Earth that can simultaneously demand your attention and ignore you completely with unmatched expertise.");
+
+        print "Constant ONE: ";
+        print constant("ONE");
+        print "<br>";
+
+        //Constant with array
+        define("SENTENCE", ["this", "is", "a", "sentence."]);
+        function sentence(){
+            //using if-else
+
+            /*STATIC $x = 0;
+            if($x == 4){
+                print "<br>";
+            }
+            else{
+                print SENTENCE[$x];
+                print " ";
+                $x++;
+            }
+            */
+
+            //using for
+            for($x = 0; $x < 4; $X++){
+                print SENTENCE[$x];
+                print " ";
+                if($x == 3)
+                    print "<br>";
+            }
+        }
+        sentence();
+
+        //Magic Constants
+        print __LINE__;
+        print "<br>";
+        print __FILE__;
+    ?>
 </body>
 </html>
