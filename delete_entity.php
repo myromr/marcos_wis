04b9551e3368d0c4a2756efd12db1e43c63065d7
Romar Marcos <romar.marcos96@gmail.com>
2024-01-18 02:11:06 +0800

    ➕ added delete entity

--- delete_entity.php
+++ delete_entity.php
@@ -3,9 +3,91 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <title>Editable Table</title>
 </head>
 <body>
-    
+    <?php
+        $servername = "localhost";
+        $username = "root";
+        $password = "";
+        $dbname = "studentrecord";
+
+        $conn = new mysqli($servername, $username, $password, $dbname);
+
+        if ($conn->connect_error) {
+            die("Connection failed: " . $conn->connect_error);
+        }
+
+        // Delete Student
+        if(isset($_POST['student_delete'])) {
+            $id = $_POST['id'];
+
+            $sql = "DELETE FROM Student WHERE StudentID = $id";
+
+            try {
+                if ($conn->query($sql) === TRUE) {
+                    echo "Deleted Student";
+                } else {
+                    echo "Error deleting entity: " . $conn->error;
+                }
+            } catch (Exception $e){
+                echo "Cannot delete student. Student is connected with other tables.";
+            }
+        }
+        
+        // Delete Course
+        if(isset($_POST['student_delete'])) {
+            $id = $_POST['id'];
+
+            $sql = "DELETE FROM Student WHERE StudentID = $id";
+
+            try {
+                if ($conn->query($sql) === TRUE) {
+                    echo "Deleted Student";
+                } else {
+                    echo "Error deleting entity: " . $conn->error;
+                }
+            } catch (Exception $e){
+                echo "Cannot delete student. Student is connected with other tables.";
+            }
+        }
+
+        // Delete Instructor
+        if(isset($_POST['student_delete'])) {
+            $id = $_POST['id'];
+
+            $sql = "DELETE FROM Student WHERE StudentID = $id";
+
+            try {
+                if ($conn->query($sql) === TRUE) {
+                    echo "Deleted Student";
+                } else {
+                    echo "Error deleting entity: " . $conn->error;
+                }
+            } catch (Exception $e){
+                echo "Cannot delete student. Student is connected with other tables.";
+            }
+        }
+
+        // Delete Enrollment
+        if(isset($_POST['student_delete'])) {
+            $id = $_POST['id'];
+
+            $sql = "DELETE FROM Student WHERE StudentID = $id";
+
+            try {
+                if ($conn->query($sql) === TRUE) {
+                    echo "Deleted Student";
+                } else {
+                    echo "Error deleting entity: " . $conn->error;
+                }
+            } catch (Exception $e){
+                echo "Cannot delete student. Student is connected with other tables.";
+            }
+        }
+
+        $conn->close();
+    ?>
 </body>
 </html>
--- search.php
+++ search.php
@@ -13,7 +13,7 @@
         $password = "";
         $dbname = "studentrecord";
 
-        $conn = new mysql($servername, $username, $password, $dbname);
+        $conn = new mysqli($servername, $username, $password, $dbname);
 
         if ($conn->connect_error) {
             die("Connection failed: " . $conn->connect_error);
