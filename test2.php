a771abc9cfdec385b28901bc82990089bc92b2d9
Romar Marcos <romar.marcos96@gmail.com>
2024-01-18 00:28:38 +0800

    ➕ search

---
+++ test2.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Search</title>
+</head>
+<body>
+    <form action="search.php" method="GET">
+        <label for="search">Search:</label>
+        <input type="text" id="search" name="search">
+        <button type="submit">Submit</button>
+    </form>
+</body>
+</html>
