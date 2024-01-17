d9f5eecf12bc94dfbaa4dc5f806b91630a115359
Romar Marcos <romar.marcos96@gmail.com>
2024-01-17 23:42:24 +0800

    ➕ actions

--- add_entity.php
+++ add_entity.php
@@ -3,7 +3,9 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <meta http-equiv="refresh" content="3;more_sql_exercise.php">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <title>Edited Table</title>
 </head>
 <body>
     
