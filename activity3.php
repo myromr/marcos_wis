93017fb57e4173f6ffe8f8c51af1678a60d24c7a
Romar Marcos <romar.marcos96@gmail.com>
2024-01-19 01:04:57 +0800

    🛠️ activity3

--- activity3.php
+++ activity3.php
@@ -38,24 +38,69 @@
             $username = $_POST['username'];
             $email = $_POST['email'];
             $edited = 0;
-        }
 
-        if ($email != null) {
-            $sql = "UPDATE users SET emial='email' WHERE id=$id";
-            $conn->query($sql);
-            $edited = 1;
-        }
-
-        if ($edited === 1) {
-            echo "Updated id = $id";
-        } else {
-            echo "Error updating entity: " . $conn->error;
+            if ($email != null) {
+                $sql = "UPDATE users SET email='$email' WHERE id=$id";
+                $conn->query($sql);
+                $edited = 1;
+            }
+    
+            if ($edited === 1) {
+                echo "Updated id = $id";
+            } else {
+                echo "Error updating entity: " . $conn->error;
+            }
         }
 
         // Delete
+        if(isset($_POST['delete'])) {
+            $id = $_POST['id'];
+
+            $sql = "DELETE FROM users WHERE id=$id";
+
+            if ($conn->query($sql) === TRUE) {
+                echo "Delete id = $id";
+            } else {
+                echo "Error deleting entity: " . $conn->error;
+            }
+        }
+
+        echo "<br><br>";
 
         // Read
+        $sql = "SELECT id, username, email FROM users";
+        $result = $conn->query($sql);
 
+        if ($result->num_rows > 0) {
+            while($row = $result->fetch_assocc()) {
+                echo "ID: " . $row["id"]. " - Username: " . $row["username"]. " - Email: " . $row["email"]. "<br>";
+            }
+        } else {
+            echo "0 results";
+        }
+
+        $conn->close();
     ?>
+
+    <form method="post" action="activity3.php">
+        <h4>CREATE</h4>
+        <input type="text" name="username" placeholder="Username">
+        <input type="text" name="email" placeholder="Email">
+        <input type="submit" name="create" value="Create">
+    </form>
+
+    <form method="post" action="activity3.php">
+        <h4>UPDATE</h4>
+        <input type="text" name="username" placeholder="Username">
+        <input type="text" name="email" placeholder="Email">
+        <input type="submit" name="create" value="Create">
+    </form>
+
+    <form method="post" action="activity3.php">
+        <h4>DELETE</h4>
+        <input type="text" name="username" placeholder="Username">
+        <input type="text" name="email" placeholder="Email">
+        <input type="submit" name="create" value="Create">
+    </form>
 </body>
 </html>
