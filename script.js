27ddff30a79e446ebd8dc998584efc1b3fa96531
Romar Marcos <romar.marcos96@gmail.com>
2024-01-19 00:04:07 +0800

    ➕ final project

--- script.js
+++ script.js
@@ -37,4 +37,162 @@ function toggleFormUsersDelete() {
         form.style.display = "none";
     }
 }
+// Student
+function toggleFormUsersCreate() {
+    var form = document.getElementById("UsersFormCreate");
+    var form1 = document.getElementById("UsersFormUpdate");
+    var form2 = document.getElementById("UsersFormDelete");
+    if (form.style.display === "none") {
+        form.style.display = "block";
+        form1.style.display = "none";
+        form2.style.display = "none";
+    } else {
+        form.style.display = "none";
+    }
+}
 
+function toggleFormUsersUpdate() {
+    var form = document.getElementById("UsersFormCreate");
+    var form1 = document.getElementById("UsersFormUpdate");
+    var form2 = document.getElementById("UsersFormDelete");
+    if (form.style.display === "none") {
+        form.style.display = "block";
+        form1.style.display = "none";
+        form2.style.display = "none";
+    } else {
+        form.style.display = "none";
+    }
+}
+
+function toggleFormUsersDelete() {
+    var form = document.getElementById("UsersFormCreate");
+    var form1 = document.getElementById("UsersFormUpdate");
+    var form2 = document.getElementById("UsersFormDelete");
+    if (form.style.display === "none") {
+        form.style.display = "block";
+        form1.style.display = "none";
+        form2.style.display = "none";
+    } else {
+        form.style.display = "none";
+    }
+}
+
+// Course
+function toggleFormUsersCreate() {
+    var form = document.getElementById("UsersFormCreate");
+    var form1 = document.getElementById("UsersFormUpdate");
+    var form2 = document.getElementById("UsersFormDelete");
+    if (form.style.display === "none") {
+        form.style.display = "block";
+        form1.style.display = "none";
+        form2.style.display = "none";
+    } else {
+        form.style.display = "none";
+    }
+}
+
+function toggleFormUsersUpdate() {
+    var form = document.getElementById("UsersFormCreate");
+    var form1 = document.getElementById("UsersFormUpdate");
+    var form2 = document.getElementById("UsersFormDelete");
+    if (form.style.display === "none") {
+        form.style.display = "block";
+        form1.style.display = "none";
+        form2.style.display = "none";
+    } else {
+        form.style.display = "none";
+    }
+}
+
+function toggleFormUsersDelete() {
+    var form = document.getElementById("UsersFormCreate");
+    var form1 = document.getElementById("UsersFormUpdate");
+    var form2 = document.getElementById("UsersFormDelete");
+    if (form.style.display === "none") {
+        form.style.display = "block";
+        form1.style.display = "none";
+        form2.style.display = "none";
+    } else {
+        form.style.display = "none";
+    }
+}
+
+// Instructor
+function toggleFormUsersCreate() {
+    var form = document.getElementById("UsersFormCreate");
+    var form1 = document.getElementById("UsersFormUpdate");
+    var form2 = document.getElementById("UsersFormDelete");
+    if (form.style.display === "none") {
+        form.style.display = "block";
+        form1.style.display = "none";
+        form2.style.display = "none";
+    } else {
+        form.style.display = "none";
+    }
+}
+
+function toggleFormUsersUpdate() {
+    var form = document.getElementById("UsersFormCreate");
+    var form1 = document.getElementById("UsersFormUpdate");
+    var form2 = document.getElementById("UsersFormDelete");
+    if (form.style.display === "none") {
+        form.style.display = "block";
+        form1.style.display = "none";
+        form2.style.display = "none";
+    } else {
+        form.style.display = "none";
+    }
+}
+
+function toggleFormUsersDelete() {
+    var form = document.getElementById("UsersFormCreate");
+    var form1 = document.getElementById("UsersFormUpdate");
+    var form2 = document.getElementById("UsersFormDelete");
+    if (form.style.display === "none") {
+        form.style.display = "block";
+        form1.style.display = "none";
+        form2.style.display = "none";
+    } else {
+        form.style.display = "none";
+    }
+}
+
+// Enrollment
+function toggleFormUsersCreate() {
+    var form = document.getElementById("UsersFormCreate");
+    var form1 = document.getElementById("UsersFormUpdate");
+    var form2 = document.getElementById("UsersFormDelete");
+    if (form.style.display === "none") {
+        form.style.display = "block";
+        form1.style.display = "none";
+        form2.style.display = "none";
+    } else {
+        form.style.display = "none";
+    }
+}
+
+function toggleFormUsersUpdate() {
+    var form = document.getElementById("UsersFormCreate");
+    var form1 = document.getElementById("UsersFormUpdate");
+    var form2 = document.getElementById("UsersFormDelete");
+    if (form.style.display === "none") {
+        form.style.display = "block";
+        form1.style.display = "none";
+        form2.style.display = "none";
+    } else {
+        form.style.display = "none";
+    }
+}
+
+function toggleFormUsersDelete() {
+    var form = document.getElementById("UsersFormCreate");
+    var form1 = document.getElementById("UsersFormUpdate");
+    var form2 = document.getElementById("UsersFormDelete");
+    if (form.style.display === "none") {
+        form.style.display = "block";
+        form1.style.display = "none";
+        form2.style.display = "none";
+    } else {
+        form.style.display = "none";
+    }
+}
