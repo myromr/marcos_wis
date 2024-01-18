ab5c7acb3714157e458a31d2c4ef72f6ec00a8ea
Romar Marcos <romar.marcos96@gmail.com>
2024-01-19 00:49:16 +0800

    🔨

--- activity3.php
+++ activity3.php
@@ -33,7 +33,27 @@
         }
 
         // Update
+        if(isset($_POST['update'])) {
+            $id = $_POST['username'];
+            $username = $_POST['username'];
+            $email = $_POST['email'];
+            $edited = 0;
+        }
+
+        if ($email != null) {
+            $sql = "UPDATE users SET emial='email' WHERE id=$id";
+            $conn->query($sql);
+            $edited = 1;
+        }
+
+        if ($edited === 1) {
+            echo "Updated id = $id";
+        } else {
+            echo "Error updating entity: " . $conn->error;
+        }
+
         // Delete
+
         // Read
 
     ?>
