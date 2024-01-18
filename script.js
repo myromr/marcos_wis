584937c6ddb1f64a1e2d870e554e8092e75d36f2
Romar Marcos <romar.marcos96@gmail.com>
2024-01-18 23:57:46 +0800

    ➕ added script

---
+++ script.js
@@ -0,0 +1,40 @@
+// Users
+function toggleFormUsersCreate() {
+    var form = document.getElementById("UsersFormCreate");
+    var form1 = document.getElementById("UsersFormUpdate");
+    var form2 = document.getElementById("UsersFormDelete");
+    if (form.style.display === "none") {
+        form.style.display = "block";
+        form1.style.display = "none";
+        form2.style.display = "none";
+    } else {
+        form.style.display = "none";
+    }
+}
+
+function toggleFormUsersUpdate() {
+    var form = document.getElementById("UsersFormCreate");
+    var form1 = document.getElementById("UsersFormUpdate");
+    var form2 = document.getElementById("UsersFormDelete");
+    if (form.style.display === "none") {
+        form.style.display = "block";
+        form1.style.display = "none";
+        form2.style.display = "none";
+    } else {
+        form.style.display = "none";
+    }
+}
+
+function toggleFormUsersDelete() {
+    var form = document.getElementById("UsersFormCreate");
+    var form1 = document.getElementById("UsersFormUpdate");
+    var form2 = document.getElementById("UsersFormDelete");
+    if (form.style.display === "none") {
+        form.style.display = "block";
+        form1.style.display = "none";
+        form2.style.display = "none";
+    } else {
+        form.style.display = "none";
+    }
+}
+
