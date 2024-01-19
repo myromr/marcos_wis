2be99ecc240512280767eed40aff5f60c61e4b0d
Romar Marcos <romar.marcos96@gmail.com>
2024-01-19 21:35:09 +0800

    🔨table_edit-delete

--- table_edit.php
+++ table_edit.php
@@ -284,6 +284,92 @@
                 }
             }
 
+            // Delete User
+            else if(isset($_POST['users_delete'])) {
+                $id = $_POST['id'];
+
+                $sql = "DELETE FROM Users WHERE UsersID = $id";
+
+                try {
+                    if($conn->query($sql) === TRUE) {
+                        echo "Deleted User";
+                    } else {
+                        echo "Error deleting entity: " . $conn->error;
+                    }
+                } catch (Exception $e) {
+                    echo "Cannot delete user.";
+                }
+            }
+
+            // Delete Student
+            else if(isset($_POST['student_delete'])) {
+                $id = $_POST['id'];
+
+                $sql = "DELETE FROM Student WHERE StudentID = $id";
+
+                try {
+                    if($conn->query($sql) === TRUE) {
+                        echo "Deleted User";
+                    } else {
+                        echo "Error deleting entity: " . $conn->error;
+                    }
+                } catch (Exception $e) {
+                    echo "Cannot delete student. Student is connected with other tables.";
+                }
+            }
+
+            // Delete Course
+            else if(isset($_POST['users_delete'])) {
+                $id = $_POST['id'];
+
+                $sql = "DELETE FROM Users WHERE UsersID = $id";
+
+                try {
+                    if($conn->query($sql) === TRUE) {
+                        echo "Deleted User";
+                    } else {
+                        echo "Error deleting entity: " . $conn->error;
+                    }
+                } catch (Exception $e) {
+                    echo "Cannot delete course. Course is connected with other tables.";
+                }
+            }
+
+            // Delete Instructor
+            else if(isset($_POST['users_delete'])) {
+                $id = $_POST['id'];
+
+                $sql = "DELETE FROM Users WHERE UsersID = $id";
+
+                try {
+                    if($conn->query($sql) === TRUE) {
+                        echo "Deleted User";
+                    } else {
+                        echo "Error deleting entity: " . $conn->error;
+                    }
+                } catch (Exception $e) {
+                    echo "Cannot delete instructor. Instructor is connected with other tables..";
+                }
+            }
+
+            // Delete Enrollment
+            else if(isset($_POST['users_delete'])) {
+                $id = $_POST['id'];
+
+                $sql = "DELETE FROM Users WHERE UsersID = $id";
+
+                try {
+                    if($conn->query($sql) === TRUE) {
+                        echo "Deleted User";
+                    } else {
+                        echo "Error deleting entity: " . $conn->error;
+                    }
+                } catch (Exception $e) {
+                    echo "Cannot delete enrollment. Enrollment is connected with other tables.";
+                }
+            }
+
+            $conn->close();
 
         ?>
     </div>
