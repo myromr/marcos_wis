4648d18f9571a44767c839002ccc3edc96f955a4
Romar Marcos <romar.marcos96@gmail.com>
2024-01-19 16:12:20 +0800

    🔨final_project, ➕search_final & table_edit

--- final_project.php
+++ final_project.php
@@ -20,10 +20,302 @@
         }
     ?>
 
-    <div>
-
+    <div class="container mb-3">
+        <form action="search_final.php" method="GET">
+            <label for="search">Search: </label>
+            <input type="text" id="search" name="search">
+            <button type="submit">Submit</button>
+        </form>
     </div>
 
     <!-- Users Table -->
+    <div class="container mb-5 mt-5">
+        <h2>Table: Users</h2>
+        <table class="table table-striped">
+            <tr>
+                <td>User ID</td> 
+                <td>Username</td> 
+                <td>Email</td> 
+                <td>Password</td> 
+            </tr>
+            <?php
+                $sqlUsers = "SELECT * FROM Users";
+                $resultUsers = $conn->query($sqlUsers);
+
+                if ($resultUsers){
+                    while ($row = $resultUsers->fetch_assoc()) {
+                        echo "<tr><td>" .$row["UserID"]. "</td>";
+                        echo "<td>" .$row["Username"]. "</td>";
+                        echo "<td>" .$row["Email"]. "</td>";
+                        echo "<td>" .$row["Passwd"]. "</td></tr>";
+                    }
+                } else {
+                    echo "Error: " . $sql . "<br>" . $conn->error;
+                }
+            ?>
+        </table>
+
+        <button onclick="toggleFormUsersCreate()">Add</button>
+        <button onclick="toggleFormUsersUpdate()">Update</button>
+        <button onclick="toggleFormUsersCreate()">Delete</button>
+
+        <div class="mt-3">
+            <form id="UsersFormCreate" method="post" action="table_edit.php" style="display: none;">
+                <h4>Add User</h4>
+                <input type="text" name="username" placeholder="Username">
+                <input type="text" name="email" placeholder="Email">
+                <input type="text" name="passwd" placeholder="Password">
+                <input type="submit" name="users_add" value="Add User">
+            </form>
+
+            <form id="UsersFormUpdate" method="post" action="table_edit.php" style="display: none;">
+                <h4>Update User</h4>
+                <input type="text" name="id" placeholder="User ID" required>
+                <input type="text" name="username" placeholder="Username">
+                <input type="text" name="email" placeholder="Email">
+                <input type="text" name="passwd" placeholder="Password">
+                <input type="submit" name="users_update" value="Update User">
+            </form>
+            
+            <form id="UsersFormDelete" method="post" action="table_edit.php" style="display: none;">
+                <h4>Delete User</h4>
+                <input type="text" name="id" placeholder="User ID" required>
+                <input type="submit" name="users_delete" value="Delete User">
+            </form>
+        </div>
+    </div>
+
+    <!-- Student Table -->
+    <div class="container mb-5 mt-5">
+        <h2>Table: Users</h2>
+        <table class="table table-striped">
+            <tr>
+                <td>User ID</td> 
+                <td>Username</td> 
+                <td>Email</td> 
+                <td>Password</td> 
+            </tr>
+            <?php
+                $sqlUsers = "SELECT * FROM Users";
+                $resultUsers = $conn->query($sqlUsers);
+
+                if ($resultUsers){
+                    while ($row = $resultUsers->fetch_assoc()) {
+                        echo "<tr><td>" .$row["UserID"]. "</td>";
+                        echo "<td>" .$row["Username"]. "</td>";
+                        echo "<td>" .$row["Email"]. "</td>";
+                        echo "<td>" .$row["Passwd"]. "</td></tr>";
+                    }
+                } else {
+                    echo "Error: " . $sql . "<br>" . $conn->error;
+                }
+            ?>
+        </table>
+
+        <button onclick="toggleFormUsersCreate()">Add</button>
+        <button onclick="toggleFormUsersUpdate()">Update</button>
+        <button onclick="toggleFormUsersCreate()">Delete</button>
+
+        <div class="mt-3">
+            <form id="UsersFormCreate" method="post" action="table_edit.php" style="display: none;">
+                <h4>Add User</h4>
+                <input type="text" name="username" placeholder="Username">
+                <input type="text" name="email" placeholder="Email">
+                <input type="text" name="passwd" placeholder="Password">
+                <input type="submit" name="users_add" value="Add User">
+            </form>
+
+            <form id="UsersFormUpdate" method="post" action="table_edit.php" style="display: none;">
+                <h4>Update User</h4>
+                <input type="text" name="id" placeholder="User ID" required>
+                <input type="text" name="username" placeholder="Username">
+                <input type="text" name="email" placeholder="Email">
+                <input type="text" name="passwd" placeholder="Password">
+                <input type="submit" name="users_update" value="Update User">
+            </form>
+            
+            <form id="UsersFormDelete" method="post" action="table_edit.php" style="display: none;">
+                <h4>Delete User</h4>
+                <input type="text" name="id" placeholder="User ID" required>
+                <input type="submit" name="users_delete" value="Delete User">
+            </form>
+        </div>
+    </div>
+    <!-- Course Table -->
+    <div class="container mb-5 mt-5">
+        <h2>Table: Users</h2>
+        <table class="table table-striped">
+            <tr>
+                <td>User ID</td> 
+                <td>Username</td> 
+                <td>Email</td> 
+                <td>Password</td> 
+            </tr>
+            <?php
+                $sqlUsers = "SELECT * FROM Users";
+                $resultUsers = $conn->query($sqlUsers);
+
+                if ($resultUsers){
+                    while ($row = $resultUsers->fetch_assoc()) {
+                        echo "<tr><td>" .$row["UserID"]. "</td>";
+                        echo "<td>" .$row["Username"]. "</td>";
+                        echo "<td>" .$row["Email"]. "</td>";
+                        echo "<td>" .$row["Passwd"]. "</td></tr>";
+                    }
+                } else {
+                    echo "Error: " . $sql . "<br>" . $conn->error;
+                }
+            ?>
+        </table>
+
+        <button onclick="toggleFormUsersCreate()">Add</button>
+        <button onclick="toggleFormUsersUpdate()">Update</button>
+        <button onclick="toggleFormUsersCreate()">Delete</button>
+
+        <div class="mt-3">
+            <form id="UsersFormCreate" method="post" action="table_edit.php" style="display: none;">
+                <h4>Add User</h4>
+                <input type="text" name="username" placeholder="Username">
+                <input type="text" name="email" placeholder="Email">
+                <input type="text" name="passwd" placeholder="Password">
+                <input type="submit" name="users_add" value="Add User">
+            </form>
+
+            <form id="UsersFormUpdate" method="post" action="table_edit.php" style="display: none;">
+                <h4>Update User</h4>
+                <input type="text" name="id" placeholder="User ID" required>
+                <input type="text" name="username" placeholder="Username">
+                <input type="text" name="email" placeholder="Email">
+                <input type="text" name="passwd" placeholder="Password">
+                <input type="submit" name="users_update" value="Update User">
+            </form>
+            
+            <form id="UsersFormDelete" method="post" action="table_edit.php" style="display: none;">
+                <h4>Delete User</h4>
+                <input type="text" name="id" placeholder="User ID" required>
+                <input type="submit" name="users_delete" value="Delete User">
+            </form>
+        </div>
+    </div>
+
+    <!-- Instructor Table -->
+    <div class="container mb-5 mt-5">
+        <h2>Table: Users</h2>
+        <table class="table table-striped">
+            <tr>
+                <td>User ID</td> 
+                <td>Username</td> 
+                <td>Email</td> 
+                <td>Password</td> 
+            </tr>
+            <?php
+                $sqlUsers = "SELECT * FROM Users";
+                $resultUsers = $conn->query($sqlUsers);
+
+                if ($resultUsers){
+                    while ($row = $resultUsers->fetch_assoc()) {
+                        echo "<tr><td>" .$row["UserID"]. "</td>";
+                        echo "<td>" .$row["Username"]. "</td>";
+                        echo "<td>" .$row["Email"]. "</td>";
+                        echo "<td>" .$row["Passwd"]. "</td></tr>";
+                    }
+                } else {
+                    echo "Error: " . $sql . "<br>" . $conn->error;
+                }
+            ?>
+        </table>
+
+        <button onclick="toggleFormUsersCreate()">Add</button>
+        <button onclick="toggleFormUsersUpdate()">Update</button>
+        <button onclick="toggleFormUsersCreate()">Delete</button>
+
+        <div class="mt-3">
+            <form id="UsersFormCreate" method="post" action="table_edit.php" style="display: none;">
+                <h4>Add User</h4>
+                <input type="text" name="username" placeholder="Username">
+                <input type="text" name="email" placeholder="Email">
+                <input type="text" name="passwd" placeholder="Password">
+                <input type="submit" name="users_add" value="Add User">
+            </form>
+
+            <form id="UsersFormUpdate" method="post" action="table_edit.php" style="display: none;">
+                <h4>Update User</h4>
+                <input type="text" name="id" placeholder="User ID" required>
+                <input type="text" name="username" placeholder="Username">
+                <input type="text" name="email" placeholder="Email">
+                <input type="text" name="passwd" placeholder="Password">
+                <input type="submit" name="users_update" value="Update User">
+            </form>
+            
+            <form id="UsersFormDelete" method="post" action="table_edit.php" style="display: none;">
+                <h4>Delete User</h4>
+                <input type="text" name="id" placeholder="User ID" required>
+                <input type="submit" name="users_delete" value="Delete User">
+            </form>
+        </div>
+    </div>
+
+    <!-- Enrollment Table -->
+    <div class="container mb-5 mt-5">
+        <h2>Table: Users</h2>
+        <table class="table table-striped">
+            <tr>
+                <td>User ID</td> 
+                <td>Username</td> 
+                <td>Email</td> 
+                <td>Password</td> 
+            </tr>
+            <?php
+                $sqlUsers = "SELECT * FROM Users";
+                $resultUsers = $conn->query($sqlUsers);
+
+                if ($resultUsers){
+                    while ($row = $resultUsers->fetch_assoc()) {
+                        echo "<tr><td>" .$row["UserID"]. "</td>";
+                        echo "<td>" .$row["Username"]. "</td>";
+                        echo "<td>" .$row["Email"]. "</td>";
+                        echo "<td>" .$row["Passwd"]. "</td></tr>";
+                    }
+                } else {
+                    echo "Error: " . $sql . "<br>" . $conn->error;
+                }
+            ?>
+        </table>
+
+        <button onclick="toggleFormUsersCreate()">Add</button>
+        <button onclick="toggleFormUsersUpdate()">Update</button>
+        <button onclick="toggleFormUsersCreate()">Delete</button>
+
+        <div class="mt-3">
+            <form id="UsersFormCreate" method="post" action="table_edit.php" style="display: none;">
+                <h4>Add User</h4>
+                <input type="text" name="username" placeholder="Username">
+                <input type="text" name="email" placeholder="Email">
+                <input type="text" name="passwd" placeholder="Password">
+                <input type="submit" name="users_add" value="Add User">
+            </form>
+
+            <form id="UsersFormUpdate" method="post" action="table_edit.php" style="display: none;">
+                <h4>Update User</h4>
+                <input type="text" name="id" placeholder="User ID" required>
+                <input type="text" name="username" placeholder="Username">
+                <input type="text" name="email" placeholder="Email">
+                <input type="text" name="passwd" placeholder="Password">
+                <input type="submit" name="users_update" value="Update User">
+            </form>
+            
+            <form id="UsersFormDelete" method="post" action="table_edit.php" style="display: none;">
+                <h4>Delete User</h4>
+                <input type="text" name="id" placeholder="User ID" required>
+                <input type="submit" name="users_delete" value="Delete User">
+            </form>
+        </div>
+    </div>
+
+    <?php
+        $conn->closed();
+    ?>
+
+    <script src="script.js"></script>
 </body>
 </html>
